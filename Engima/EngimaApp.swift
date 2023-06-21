6a4db29078ad9c35b4ddfefa1283f65f15c5bc21
manorajesh <mano@raje.sh>
2023-06-20 23:10:44 -0700

    Initial Commit

---
+++ Engima/EngimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngimaApp.swift
+//  Engima
+//
+//  Created by Mano Rajesh on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct EngimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
